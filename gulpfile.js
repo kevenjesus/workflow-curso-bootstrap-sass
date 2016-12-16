93bd299690b28a2ec67fcf15c1ac90b8c0186e21
Keven jesus <keven@Keven-MacBook-Pro.local>
2016-12-15 22:29:14 -0200

    default

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,14 @@ const gulp 		= require("gulp");
 const sass 		= require("gulp-sass");
 const notify 	= require("gulp-notify");
 
-gulp.task("default", function(){
+gulp.task("scss", function(){
 	return gulp.src(["./*.scss","./*.sass"])
 				.pipe(sass())
 				.pipe(gulp.dest("./"))
 });
+
+gulp.task('watch', function(){
+	gulp.watch('./*.scss', ['scss']);
+});
+
+gulp.task("default", ['scss', 'watch']);
